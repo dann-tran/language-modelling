c793d48536fe9d7d96438b0b56420dced126f10a
picas9dan <picasdan99@gmail.com>
2024-01-14 09:41:57 +0800

    Pretokenize wikipedia corpus

---
+++ ngram/pretokenize.py
@@ -0,0 +1,25 @@
+import os
+import pandas as pd
+from nltk.tokenize.treebank import TreebankWordTokenizer
+from multiprocessing import Pool
+from tqdm import tqdm
+
+tokenizer = TreebankWordTokenizer()
+
+
+def save_tokens(filepath: str):
+    df = pd.read_parquet(filepath)
+    df["tokens"] = df["text"].str.lower().apply(tokenizer.tokenize)
+    df.to_parquet(filepath)
+
+
+DIRPATH = "/Users/danntran/Repos/language-modelling/data/wikpedia-20231101.en"
+FILENAMES = [fn for fn in os.listdir(DIRPATH) if fn.endswith(".parquet")]
+FILEPATHS = [os.path.join(DIRPATH, fn) for fn in FILENAMES]
+
+if __name__ == "__main__":
+    with Pool(8) as pool:
+        for _ in tqdm(
+            pool.imap_unordered(save_tokens, FILEPATHS), total=len(FILEPATHS)
+        ):
+            pass
