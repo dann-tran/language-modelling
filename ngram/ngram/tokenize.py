e7bd20bcd101fbe5676ebc38bd598f2f3dd725a5
picas9dan <picasdan99@gmail.com>
2024-02-07 21:51:34 +0700

    Update tokenize function to handle non-ASCII characters

--- ngram/ngram/tokenize.py
+++ ngram/ngram/tokenize.py
@@ -1,10 +1,16 @@
-PUNCTUATIONS = set('.?!,:;(){}[]"-–—@#$%&')  # hyphen, en-dash, em-dash
+import json
+from typing import Iterable
+
+
+PUNCTUATIONS = set('.?!,:;(){}[]"-–—@#$%&|=~></\\')  # hyphen, en-dash, em-dash
 CONTRACTIONS = ["n't", "'ve", "'d", "'ll", "'s", "'re", "'m"]
 POSSESSIVE_APOS = ["'s", "'"]
 SUFFIX_TOKENS = CONTRACTIONS + POSSESSIVE_APOS
 
 
 def tokenize(text: str):
+    # replace non-ASCII characters with whitespaces
+    text = "".join([c.lower() if c.isascii() else " " for c in text])
     tokens = []
     idx = 0
 
@@ -21,6 +27,14 @@ def tokenize(text: str):
             continue
 
         idx_forward = idx + 1
+
+        if text[idx].isnumeric():
+            while idx_forward < len(text) and text[idx_forward].isnumeric():
+                idx_forward += 1
+            tokens.append(text[idx:idx_forward])
+            idx = idx_forward
+            continue
+
         while (
             idx_forward < len(text)
             and not text[idx_forward].isspace()
@@ -49,3 +63,31 @@ def tokenize(text: str):
         idx = idx_forward
 
     return tokens
+
+
+class Tokenizer:
+    BOS_TOKEN = "<s>"
+    EOS_TOKEN = "</s>"
+    UNK_TOKEN = "<UNK>"
+    SPECIAL_TOKENS = [BOS_TOKEN, EOS_TOKEN, UNK_TOKEN]
+
+    def __init__(self, vocab: Iterable[str]):
+        vocab = list(vocab)
+        vocab.sort()
+        self.tokens = self.SPECIAL_TOKENS + vocab
+        self.tok2id = {tok: i for i, tok in enumerate(self.tokens)}
+        self.vocab = set(self.tokens)
+
+    def save(self, filepath: str):
+        with open(filepath, "w") as f:
+            json.dump({i: tok for i, tok in enumerate(self.vocab)}, f, indent=4)
+
+    def tokenize(self, text: str):
+        ids = [self.tok2id[self.BOS_TOKEN]]
+
+        for tok in tokenize(text):
+            ids.append(self.tok2id.get(tok, self.tok2id[self.UNK_TOKEN]))
+
+        ids.append(self.tok2id[self.EOS_TOKEN])
+
+        return ids
